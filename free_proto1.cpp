f8e0bc423773866d7b9913b101c8f17d9530fd09
freekani <freekanid@gmail.com>
2018-10-25 11:46:29 +0900

    @free
    読みやすくした

--- free_proto1.cpp
+++ free_proto1.cpp
@@ -9,17 +9,21 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < 10; i++)
     {
+        //ガウス・ルジャンドル法を利用
         a1 = (a + b) / 2.0;
-        b1 = sqrt(a * b);
-        t1 = t - p * (a - a1) * (a - a1);
+        b1 = sqrt(a * b);                 //ここの精度後々不安すぎる
+        t1 = t - p * (a - a1) * (a - a1); //ここで整合性が取れなくなるので計算の順番は変えないこと
         p1 = 2.0 * p;
 
         a = a1;
         b = b1;
         t = t1;
         p = p1;
-
-        cout << setprecision(200) << a << " " << b << " " << t << " " << p << " " << (a + b) * (a + b) / 4.0 / t << endl;
+        //cout<<fixed;
+        //これを入れると未計算の桁が0埋めされる
+        cout << setprecision(200) //桁数はiomanipで指定してるよ
+             << a << " " << b << " " << t << " " << p << " " << (a + b) * (a + b) / 4.0 / t << endl;
+        //答えを求める公式(a + b) * (a + b) / 4.0 / t
     }
     return 0;
 }
