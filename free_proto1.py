e272d8fdf6cfc3ab679cb36cb670620226eb9322
freekani <freekanid@gmail.com>
2018-10-31 12:44:57 +0900

    @free
    w

--- free_proto1.py
+++ free_proto1.py
@@ -3,14 +3,11 @@ a = 1.0
 b = math.sqrt(2.0) / 2.0
 t = 0.25
 p = 1.0
-a1 = 1.0
-b1 = 1.0 / math.sqrt(2.0) / 2.0
-t1 = 0.25
-p1 = 1.0
+
 for i in range(4):
     a1 = (a + b) / 2.0
     b1 = math.sqrt(a * b)
-    t1 = t - p * (a - a1) * (a - a1)
+    t1 = t - p * (a - a1) **2
     p1 = 2.0 * p
 
     a = a1
@@ -18,4 +15,4 @@ for i in range(4):
     t = t1
     p = p1
 
-    print((a + b) * (a + b) / 4.0 / t)
+    print((a + b) **2 / 4.0 / t)
