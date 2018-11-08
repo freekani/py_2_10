488d193c72bd16d8f0389412fbdf892baa5ed8b5
freekani <freekanid@gmail.com>
2018-11-08 12:24:07 +0900

    @free
    もう実装には触りません

--- free_proto1.py
+++ free_proto1.py
@@ -1,5 +1,5 @@
 from decimal import *
-getcontext().prec = 1000
+getcontext().prec = 5000
 
 
 a = Decimal(1.0)
@@ -7,15 +7,14 @@ b = Decimal(2.0).sqrt() / Decimal(2.0)
 t = Decimal(0.25)
 p = Decimal(1.0)
 
-for i in range(5):
+for i in range(100):
     a1 = (a + b) / Decimal(2.0)
     b1 = Decimal(a * b).sqrt()
     t1 = t - p * (a - a1) ** Decimal(2)
-    p1 = Decimal(2.0) * p
 
     a = a1
     b = b1
     t = t1
-    p = p1
+    p = Decimal(2.0) * p
 
-    print((a + b) ** Decimal(2) / Decimal(4.0) / t)
+print((a + b) ** Decimal(2) / Decimal(4.0) / t)
