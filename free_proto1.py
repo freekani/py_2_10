4c3d9e48425349ff840305a063d737efb9d75385
freekani <freekanid@gmail.com>
2018-11-08 14:31:24 +0900

    @free
    コメントアウトを追加しました

--- free_proto1.py
+++ free_proto1.py
@@ -1,13 +1,14 @@
 from decimal import *
-getcontext().prec = 5000
-
+import time
+getcontext().prec = 5000    #ここで桁数を調整
+starttime = time.clock()
 
 a = Decimal(1.0)
 b = Decimal(2.0).sqrt() / Decimal(2.0)
 t = Decimal(0.25)
 p = Decimal(1.0)
 
-for i in range(100):
+for i in range(100):    #log2(桁数)回より多めに繰り返しましょう
     a1 = (a + b) / Decimal(2.0)
     b1 = Decimal(a * b).sqrt()
     t1 = t - p * (a - a1) ** Decimal(2)
@@ -17,4 +18,9 @@ for i in range(100):
     t = t1
     p = Decimal(2.0) * p
 
+endtime = time.clock()
 print((a + b) ** Decimal(2) / Decimal(4.0) / t)
+
+time = endtime - starttime
+
+print("実行時間:%f" % time)
